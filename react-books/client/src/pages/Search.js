90aa2d3b00d72f6f810e54b20ff8939827f157b4
Ben Olsen <olsenben1992@gmail.com>
2020-04-25 16:11:30 -0700

    deployment

--- react-books/client/src/pages/Search.js
+++ react-books/client/src/pages/Search.js
@@ -1,9 +1,11 @@
 import React from "react";
+import API from "../utils/API";
 export default ({ setInput, searchSubmit, books }) => {
   return (
     <>
       <h1>Search Google Books</h1>
       <input onChange={(e) => setInput(e.target.value)} />
+
       <button className="btn btn-warning" onClick={() => searchSubmit()}>
         Submit
       </button>
